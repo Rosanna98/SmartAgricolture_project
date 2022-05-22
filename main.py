dd9e9b8f3c34c2e00a58f8163aaee7705fa1b640
rosanna98 <240189@studenti.unimore.it>
2022-05-22 20:47:54 +0200

    graph

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from google.cloud import firestore
 from secret import secret
-
+import json #consente di convertire oggetti in stringhe in formato json
 app = Flask(__name__)
 @app.route('/',methods=['GET'])
 def main():
@@ -10,10 +10,19 @@ def main():
 @app.route('/sensors/sensor1', methods =['GET']) #metodo che permette di visualizzare tutti i dati
 def read_all():
     db=firestore.Client()
+    data=[]
     result=''
     for doc in db.collection('sensor1').stream():
-        result += (f'{doc.id} --> {doc.to_dict()} <br>')
-    return result
+        x = doc.to_dict()
+        data.append([x['Date'],float(x['Humidity'])])
+    return json.dumps(data) #bisogna che ritorni una stringa affichè il broswer lo possa leggere quindi usiamo json
+
+@app.route('/graph', methods =['GET'])
+def graph():
+    #riconverto di nuovo in lista di liste in modo che funzioni l'"insert"
+    data = json.loads(read_all()) #questa funzione chiama la funzione di sopra e si fa passare i dati numerici
+    data.insert(0,['Date', 'Humidity'])
+    return render_template('graph.html',data=data)
 
 @app.route('/sensors/sensor1', methods =['POST']) #questo metodo salva nella tabelal 'sensor1' il timestamp e i dati
 def save_data():
