e28267ad8e6544fd35f5094505a78b1b280e4f55
rosanna98 <240189@studenti.unimore.it>
2022-05-22 11:54:43 +0200

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from google.cloud import firestore
-#from secret import secret
+from secret import secret
 
 app = Flask(__name__)
 @app.route('/',methods=['GET'])
@@ -17,20 +17,20 @@ def read_all():
 
 @app.route('/sensors/sensor1', methods =['POST']) #questo metodo salva nella tabelal 'sensor1' il timestamp e i dati
 def save_data():
-    #s =request.values['secret']
-    #if s == secret:
-    db=firestore.Client()
-
-    Date = request.values['Date']
-    MaxT = request.values['MaxT']
-    MinT = request.values['MinT']
-    WindSpeed = request.values['WindSpeed']
-    Humidity = request.values['Humidity']
-    Precipitation = request.values['Precipitation']
-    db = firestore.Client()
-    #per evitare che se si blocca l'invio dei dati e dopo ricomincia evito che si creino duplicati
-    db.collection('sensor1').document(Date).set({'Date': Date, 'MaxT': MaxT, 'MinT':MinT, 'WindSpeed': WindSpeed, 'Humidity': Humidity, 'Precipitation': Precipitation})
-
+    s = request.values['secret']
+    if s == secret:
+        Date = request.values['Date']
+        MaxT = request.values['MaxT']
+        MinT = request.values['MinT']
+        WindSpeed = request.values['WindSpeed']
+        Humidity = request.values['Humidity']
+        Precipitation = request.values['Precipitation']
+        db = firestore.Client()
+        #per evitare che se si blocca l'invio dei dati e dopo ricomincia evito che si creino duplicati
+        db.collection('sensor1').document(Date).set({'Date': Date, 'MaxT': MaxT, 'MinT':MinT, 'WindSpeed': WindSpeed, 'Humidity': Humidity, 'Precipitation': Precipitation})
+        return "ok", 200
+    else:
+        return "", 401
 
 
 if __name__ == '__main__':
